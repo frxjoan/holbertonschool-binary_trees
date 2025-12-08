f34f57dcc5b647fbb93c4e20cc25ece8a70e1de8
Safwann8 <safoulefou83@gmail.com>
2025-12-08 16:09:17 +0000

    correction the file 9 because not all check passing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,17 +13,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int height_left = 0;
-	int height_right = 0;
-	
+	size_t height_left = 0;
+	size_t height_right = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
+	if (tree->left)
+		height_left = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		height_right = 1 + binary_tree_height(tree->right);
 
 	if (height_left > height_right)
-		return (1 + height_left);
-	else
-		return (1 + height_right);
+		return height_left;
+	return height_right;
 }
