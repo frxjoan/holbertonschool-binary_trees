4f182dd19aa9c5ed9e5ac4f9b5b2b5cd96a7fee4
frxjoan <12224@holbertonstudents.com>
2025-12-08 11:55:37 +0100

    12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include <stdlib.h> 
+/**
+ * binary_tree_depth - create a binary tree
+ * @tree: node
+ * Return: a pointer to the new binary tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
