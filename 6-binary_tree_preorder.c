5bdfbebaa0caebb4a624e1cda2ad69cbcc462c11
frxjoan <12224@holbertonstudents.com>
2025-12-08 10:45:18 +0100

    6 and 8

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_preorder - create a binary tree
+ * @tree: p
+ * @func: value
+ * Return: void
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
