13e9281d0c4feb9032d73190f37a036eeb7875a8
frxjoan <12224@holbertonstudents.com>
2025-12-09 15:02:43 +0100

    adv 2

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,55 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_height - create a binary tree
+ * @tree: node
+ * Return: a pointer to the new binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left > right)
+		return (1 + left);
+	return (1 + right);
+}
+/**
+ * binary_tree_preorder - create a binary tree
+ * @tree: p
+ * @func: value
+ * Return: void
+ */
+void binary_tree_print_level(const binary_tree_t *tree, size_t level, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (level == 0)
+		func(tree->n);
+	else if (0 < level)
+	{
+		binary_tree_print_level(tree->left, level - 1, func);
+		binary_tree_print_level(tree->right, level - 1, func);
+	}
+}
+/**
+ * binary_tree_preorder - create a binary tree
+ * @tree: p
+ * @func: value
+ * Return: void
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t i;
+
+	if (tree == NULL || func == NULL)
+		return;
+	for (i = 0; i < binary_tree_height(tree); i++)
+	{
+		binary_tree_print_level(tree, i, func);
+	}
+}
